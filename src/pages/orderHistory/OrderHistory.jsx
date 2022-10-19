122a389134da33b52f3585306fd9b42c9fbfa9c4
Oğulcan Doğan <odogan@Ogulcan-MacBook-Air.local>
2022-10-19 13:51:54 +0300

    finished :tada:

--- src/pages/orderHistory/OrderHistory.jsx
+++ src/pages/orderHistory/OrderHistory.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import styles from "./OrderHistory.module.scss";
+import shoes from "../../images/zara-ayakkabi.jpg";
+import phone_1 from "../../images/IPHONE-11.png";
+import phone_2 from "../../images/iphone_12.png";
+import phone_3 from "../../images/iphone-11-red.jpeg";
 
 function OrderHistory() {
   return (
@@ -14,6 +18,7 @@ function OrderHistory() {
                 <thead>
                   <tr>
                     <th></th>
+                    <th>Ürün</th>
                     <th>Sipariş Tarihi</th>
                     <th>Sipariş ID</th>
                     <th>Sipariş Durumu</th>
@@ -22,6 +27,12 @@ function OrderHistory() {
                 <tbody>
                   <tr>
                     <td>1</td>
+                    <td>
+                      <img
+                        src={shoes}
+                        style={{ width: "150px", height: "120px" }}
+                      />
+                    </td>
                     <td>19.10.2022 14:53</td>
                     <td>Brr5dSjt3adYkUPqi00Z</td>
                     <td>
@@ -30,6 +41,12 @@ function OrderHistory() {
                   </tr>
                   <tr>
                     <td>2</td>
+                    <td>
+                      <img
+                        src={phone_1}
+                        style={{ width: "150px", height: "120px" }}
+                      />
+                    </td>
                     <td>19.10.2022 14:53</td>
                     <td>Brr5dSjt3adYkUPqi00Z</td>
                     <td>
@@ -38,6 +55,13 @@ function OrderHistory() {
                   </tr>
                   <tr>
                     <td>3</td>
+                    <td>
+                      {" "}
+                      <img
+                        src={phone_2}
+                        style={{ width: "150px", height: "120px" }}
+                      />
+                    </td>
                     <td>19.10.2022 14:53</td>
                     <td>Brr5dSjt3adYkUPqi00Z</td>
                     <td>
@@ -46,6 +70,16 @@ function OrderHistory() {
                   </tr>
                   <tr>
                     <td>4</td>
+                    <td>
+                      <img
+                        src={phone_3}
+                        style={{
+                          width: "100px",
+                          height: "120px",
+                          marginLeft: "25px",
+                        }}
+                      />
+                    </td>
                     <td>19.10.2022 14:53</td>
                     <td>Brr5dSjt3adYkUPqi00Z</td>
                     <td>
